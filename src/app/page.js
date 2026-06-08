f6d288c37134bc6f04d6b40212b636d6738995fd
francisjolaoso1-dev <francisjolaoso1@gmail.com>
2026-06-08 09:26:24 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -50,7 +50,7 @@ export default function Home() {
   const handleAuthSubmit = (e) => {
     e.preventDefault();
     if (authView === 'register') {
-      const newUser = {alert('Success! Your account has been registered.');
+      const newUser = {
         name: authName || authEmail.split('@')[0] || 'User',
         email: authEmail,
         accountType: authTier,
